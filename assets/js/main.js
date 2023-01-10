d8f496b4f527b92628725993b5a66d6a24e61a54
NedSInt <pachecojhonatan12@gmail.com>
2023-01-09 21:48:17 -0300

    Update Api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
 
-const offset = 0
-const limit = 10
-
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon){
     return `
             <li class="pokemon">
@@ -25,21 +20,8 @@ function convertPokemonToLi(pokemon){
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-
-pokeApi.getPokemons().then((pokemons) => {
-    const listItems = []
-
-
-    pokemons.map()
-
-
-
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        listItems.push(convertPokemonToLi(pokemon))
-    }
-
-    console.log(listItems)
+pokeApi.getPokemons().then((pokemons = []) => {
+     const newHtml = pokemons.map(convertPokemonToLi).join('')
+     pokemonList.innerHTML += newHtml
 })
 
